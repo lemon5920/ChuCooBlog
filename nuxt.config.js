17a76b10ee22b93b79009c6e5a2107d593b2418e
lemon5920 <lemon5920@gmail.com>
2017-06-16 18:19:18 +0800

    設計預設頁面、清掉範例 code 引入 bulma 放點假資料 Blog 首頁設計一下（文章 List）

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,11 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href:'https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css' }
+    ],
+    script: [
+      { src: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js' }
     ]
   },
   /*
--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,104 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        NUXT
-      </h1>
-      <h2 class="subtitle">
-        Universal Vue.js Application
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">Documentation</a>
-        <a href="https://github.com/nuxt/nuxt.js" target="_blank" class="button--grey">GitHub</a>
+  <div>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            ChuCooBlog
+          </h1>
+          <h2 class="subtitle">
+            戳戳你的 Blog
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <div class="container articleList">
+      <div class="card" v-for="item in list">
+        <header class="card-header">
+          <p class="card-header-title">
+            {{ item.title }}
+          </p>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            {{ item.content }}
+            <br>
+            <small><span class="author-name">{{ item.author.name }}</span> - {{ item.updated_at }}</small>
+            <br>
+            <span class="tag is-warning" v-for="tag in item.tags">#{{ tag }}</span>
+          </div>
+        </div>
+        <footer class="card-footer">
+          <a class="card-footer-item">More...</a>
+        </footer>
       </div>
     </div>
-  </section>
+  </div>
 </template>
 
 <script>
-import Logo from '~components/Logo.vue'
-
 export default {
-  components: {
-    Logo
+  data () {
+    return {
+      list: [
+        {
+          'id': 1,
+          'title': '我來我見我吃飯',
+          'content': '此地共至也推門中特感子那位？決絕自時感，著到驗從背教成食、長雨為，連爸令的源，招風入在洋成青希，樹魚美，體海親雲，地價面南。受變時的類人了高落；件前綠果什大求身院，難每品供感候坐我家語熱體近落一發。過也完省情無東品這近的受論，差象提美英那。香水來組戰香不！教海到保，立委明呢！快到只本難前議發圖如何化笑次過部放大師就雙看過望小：說受子電子考建學於三。',
+          'created_at': '2017-06-16T06:25:08+00:00',
+          'updated_at': '2017-06-16T06:25:08+00:00',
+          'author': {
+            'username': 'lemon',
+            'name': 'lemon liao',
+            'gender': 'm',
+            'address': '南投縣埔里鎮大學路1號'
+          },
+          'tags': ['至理名言', '短文集']
+        },
+        {
+          'id': 2,
+          'title': 'hehe',
+          'content': '我餓了',
+          'created_at': '2017-06-17T06:25:08+00:00',
+          'updated_at': '2017-06-18T06:25:08+00:00',
+          'author': {
+            'username': 'lemon',
+            'name': 'lemon casa',
+            'gender': 'f',
+            'address': '南投縣埔里鎮大學路100號'
+          },
+          'tags': ['至理名言', '廢文集']
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.articleList {
+  padding-top: 20px;
 }
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+
+.author-name {
+  font-size: 15px;
+  font-style: italic;
 }
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+
+.card {
+  margin-top: 20px;
 }
-.links
-{
-  padding-top: 15px;
+
+.content {
+  white-space: nowrap; 
+  width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis; 
+}
+
+.tag {
+  margin-top: 10px;
+  margin-right: 5px;
 }
 </style>
